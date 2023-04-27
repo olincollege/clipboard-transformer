3d3ccf674813cf7e1cda18bd6ab27dbb72e55eb7
Sam <iamtheyammer@gmail.com>
2023-04-27 19:48:19 -0400

    remove committed build dir

--- build/test/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/anmolsandhu/github/courses/clipboard-transformer/test
-# Build directory: /Users/anmolsandhu/github/courses/clipboard-transformer/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
